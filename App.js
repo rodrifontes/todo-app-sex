583d1826b5a251d89d05eeba3e4587ef6b3f5d5b
Rodrigo Fontes <rodrigo.cruz@ifs.edu.br>
2024-10-04 12:06:28 -0300

    Aula 01: Criação Projeto e configuração de fontes customizadas

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+
+import { SafeAreaView, StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+  });
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={{ fontFamily: 'GeneralSans-400' }}>GeneralSans-400</Text>
+      <Text style={{ fontFamily: 'GeneralSans-600' }}>GeneralSans-600</Text>
+      <Text style={{ fontFamily: 'GeneralSans-700' }}>GeneralSans-700</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
 });
